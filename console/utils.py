2b5e22b888a7b183af4d74a9032067fd01cdbd5e
Francesco Leacche <francescoleacche@gmail.com>
2014-08-06 13:28:51 +0200

    now all_days_of doesn't return dates in the future when requesting current month or year

--- console/utils.py
+++ console/utils.py
@@ -1,5 +1,4 @@
-from django.utils.dates import MONTHS
-from datetime import date
+from datetime import date, timedelta
 from calendar import monthrange
 
 
@@ -12,13 +11,28 @@ def all_days_of(year, month=None, day=None):
         if day and month:
             res.append(date(year, month, day))
         elif month:
-            for d in xrange(monthrange(year, month)[1]):
+            today = date.today()
+            if year == today.year and month == today.month:
+                last_day = today.day
+            else:
+                last_day = monthrange(year, month)[1]
+
+            for d in xrange(last_day):
                 res.extend(all_days_of(year, month, d+1))
         else:
-            for m in MONTHS.keys():
+            today = date.today()
+
+            last_month = 12 if year != today.year else today.month
+
+            for m in xrange(1, last_month+1):
                 res.extend(all_days_of(year, m))
         return res
 
 
-def exluded_days(all_days, days_to_exclude):
+def excluded_days(all_days, days_to_exclude):
     return diff(all_days, days_to_exclude)
+
+
+def daterange(start_date, end_date):
+    for n in xrange((end_date - start_date).days + 1):
+        yield start_date + timedelta(n)
