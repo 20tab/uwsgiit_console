4be8981e40302725f59306ace22c527b5f52f5ba
Francesco Leacche <francescoleacche@gmail.com>
2014-09-01 17:05:09 +0200

    upgraded metrics scripts

--- static/js/metrics.js
+++ static/js/metrics.js
@@ -4,12 +4,22 @@ var metrics_list = [];
 
 var shared_graph;
 
+var last_graph_time_unit;
+
 function clearGraph() {
-  $('#legend').empty();
-  $('#chart_container').html('<div id="chart"></div><div id="timeline"></div><div id="legend_container"><div id="smoother" title="Smoothing"></div><div id="legend"></div></div><div id="slider"></div>');
-  shared_graph = undefined;
-  metrics_list = [];
-  palette = new Rickshaw.Color.Palette();
+    $('#legend').empty();
+    $('#chart_container').html(
+        '<div id="chart"></div>\
+        <div id="timeline"></div>\
+        <div id="legend_container">\
+            <div id="smoother" title="Smoothing"></div>\
+            <div id="legend"></div>\
+        </div>\
+        <div id="slider"></div>');
+    shared_graph = undefined;
+    last_graph_time_unit = undefined;
+    metrics_list = [];
+    palette = new Rickshaw.Color.Palette();
 }
 
 
@@ -59,7 +69,21 @@ function parseTimestamps(list, absoluteValues, unitOfMeasure, timeUnit){
     for(var el in data){
         res.push({x: parseInt(el), y: data[el]});
     }
-    console.log(res);
+
+    var fixed_length;
+
+    if (timeUnit == 'hour'){
+        fixed_length = 24;
+    }
+    else if(timeUnit == 'day'){
+        fixed_length = 31;
+    }
+    else if(timeUnit == 'month'){
+        fixed_length = 12;
+    }
+    while(res.length < fixed_length){
+        res.push({x: res.length, y: undefined});
+    }
     return res;
 }
 
@@ -74,6 +98,12 @@ $(document).ready(function() {
             data: frm.serialize(),
             dataType: 'json',
             success: function (data) {
+                if (last_graph_time_unit != undefined && last_graph_time_unit != data['time_unit']){
+                    clearGraph();
+                }
+
+                last_graph_time_unit = data['time_unit'];
+
                 data['stats'] = parseTimestamps(
                     data['stats'],
                     data['absolute_values'],
@@ -83,18 +113,43 @@ $(document).ready(function() {
 
                 var metrics = {
                     color: palette.color(),
+                    stroke: 'rgba(0,0,0,0.15)',
                     data: data['stats'],
                     name: data['metric_name']
                 };
 
                 metrics_list.push(metrics);
 
-                var graph = new Rickshaw.Graph( {
-                    element: document.getElementById('chart'),
-                    renderer: 'line',
+                var chart_id;
+
+                if (shared_graph != undefined){
+                    $('#chart_container').append(
+                        '<div id="chart-' + data['metric_name'] + '"></div>\
+                        <div id="timeline-' + data['metric_name'] + '"></div>\
+                        <div id="legend_container-' + data['metric_name'] + '">\
+                            <div id="smoother-' + data['metric_name'] + '" title="Smoothing"></div>\
+                            <div id="legend-' + data['metric_name'] + '"></div>\
+                        </div>\
+                        <div id="slider-' + data['metric_name'] + '"></div>'
+                    );
+                    chart_id = 'chart-' + data['metric_name'];
+                    legend_id = '#legend-' + data['metric_name'];
+
+                }
+                else{
+                    chart_id = 'chart';
+                    legend_id = '#legend';
+                }
+
+                var graph = new Rickshaw.Graph({
+                    element: document.getElementById(chart_id),
+                    renderer: 'area',
+                    stroke: true,
                     height: 300,
                     series: [metrics]
                 });
+
+                var legend_id;
 
                 if (shared_graph != undefined){
                     for (i in metrics_list){
@@ -104,6 +159,19 @@ $(document).ready(function() {
                         shared_graph.series.pop();
                     }
                     shared_graph.update();
+                    $('#legend_container').empty();
+                    var shared_graph_legend = new Rickshaw.Graph.Legend({
+                        graph: shared_graph,
+                        element: document.getElementById('legend_container')
+                    });
+                    var shelving = new Rickshaw.Graph.Behavior.Series.Toggle({
+                        graph: shared_graph,
+                        legend: shared_graph_legend
+                    });
+                    var highlighter = new Rickshaw.Graph.Behavior.Series.Highlight({
+                        graph: shared_graph,
+                        legend: shared_graph_legend
+                    });
                 }
                 else{
                     shared_graph = graph;
@@ -120,17 +188,7 @@ $(document).ready(function() {
 
                 var legend = new Rickshaw.Graph.Legend({
                     graph: graph,
-                    element: document.querySelector('#legend')
-                });
-
-                var shelving = new Rickshaw.Graph.Behavior.Series.Toggle({
-                    graph: graph,
-                    legend: legend
-                });
-
-                var highlighter = new Rickshaw.Graph.Behavior.Series.Highlight({
-                    graph: graph,
-                    legend: legend
+                    element: document.querySelector(legend_id)
                 });
 
                 var timeFixture = new Rickshaw.Fixtures.Time();
