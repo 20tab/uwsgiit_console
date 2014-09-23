899c6bc1745e35cbd0e85d969da43daeb52f4317
Francesco Leacche <francescoleacche@gmail.com>
2014-09-23 16:54:40 +0200

    fixed demo project

--- demo/demo/wsgi.py
+++ demo/demo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for uwsgiit_console project.
+WSGI config for demo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uwsgiit_console.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "demo.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
