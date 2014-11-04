3a3830000798487b54d682fef8e2302fc776e524
Francesco Leacche <francescoleacche@gmail.com>
2014-11-04 16:29:47 +0100

    fix latest_alarms script to set the new latest alarm id

--- console/static/console/js/latest_alarms.js
+++ console/static/console/js/latest_alarms.js
@@ -1,3 +1,19 @@
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+
 if (("Notification" in window)) {
     var permission = Notification.permission;
     if (permission != "granted") {
@@ -16,15 +32,17 @@ if (("Notification" in window)) {
             success: function (data) {
                 var i = 0;
                 while (data[i]['id'] != last_alarm_id) {
-                    var notification = new Notification('New alarm!', {'icon': 'icon.png', 'body': data[i]['msg']});
+                    color = hexToRgb(data[i]['color']);
+                    var notification = new Notification('New alarm!', {'icon': '/gif_' +color.r+ '_' +color.g+ '_' +color.b+ '.gif', 'body': data[i]['msg']});
                     notification.onclick = function() {
                         window.location = alarm_url;
                     }
                     i++;
                 }
+                last_alarm_id = data[0]['id'];
             },
             complete: function() {
-                setTimeout(get_latest_alarms, 180000);
+                setTimeout(get_latest_alarms, 1800);
             }
         });
     }
