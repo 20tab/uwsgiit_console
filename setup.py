aefd973fb2a47a2e92fc8c560b42d5a3dd64f097
Francesco Leacche <francescoleacche@gmail.com>
2014-12-23 10:29:09 +0100

    add python3 compatibility

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "Framework :: Django",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Programming Language :: JavaScript",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
